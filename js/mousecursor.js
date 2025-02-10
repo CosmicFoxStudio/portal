401bc04268fb49dd6625f1e000838686f72b158b
Alex Silva <desufg826@gmail.com>
2025-02-10 01:31:36 -0300

    card de cada jogo e interação com os botões

--- js/mousecursor.js
+++ js/mousecursor.js
@@ -197,3 +197,28 @@
       return ("rgb("+c[0]+", "+c[1]+", "+c[2]+")");
     }
     // ]]>
+
+    //AÇÃO DOS BOÕTD QUE AVANÇAM E RETROCEDEM OS CARDS
+    document.addEventListener("DOMContentLoaded", function () {
+      const track = document.querySelector(".carousel-track");
+      let index = 0;
+  
+      window.nextSlide = function() {
+          const cards = document.querySelectorAll(".game-card");
+          const cardWidth = cards[0].offsetWidth + 20; // Inclui o espaçamento
+          if (index < cards.length - 1) {
+              index++;
+              track.style.transform = `translateX(-${index * cardWidth}px)`;
+          }
+      };
+  
+      window.prevSlide = function() {
+          const cards = document.querySelectorAll(".game-card");
+          const cardWidth = cards[0].offsetWidth + 20;
+          if (index > 0) {
+              index--;
+              track.style.transform = `translateX(-${index * cardWidth}px)`;
+          }
+      };
+  });
+  
